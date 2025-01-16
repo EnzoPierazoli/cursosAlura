8983b8596cdab7d85beca4f199c3533bdeff26be
Enzo Pierazoli Guerra De Moraes <enzopierazoli@gmail.com>
2025-01-16 10:59:55 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+alert("Boas vindas ao jogo do numero secreto");
+
+let numeroMaximo = 100;
+let numeroSecreto = parseInt(Math.random() * (numeroMaximo + 1));
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um numero entre ${numeroMaximo}`);
+
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O numero secreto e menor que o ${chute}`);
+        } else {
+            alert(`O numero secreto e maior que ${chute}`);
+        }
+        tentativas++;
+    }
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Voce descobriu o numero secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
