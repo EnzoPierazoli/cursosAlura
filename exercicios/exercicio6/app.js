47a5389801f0ac9aa6e4a7cf3983d4591e348f27
Enzo Pierazoli Guerra De Moraes <enzopierazoli@gmail.com>
2025-01-21 09:13:00 -0300

    projeto2/exercicios6

---
+++ exercicios/exercicio6/app.js
@@ -0,0 +1,11 @@
+listaGenerica = [];
+
+linguagensDeProgramacao = [`JavaScript`,`C`,`C++`,`Kotlin`,`Pyton`];
+
+linguagensDeProgramacao.push(`Java`, `Ruby`, `GoLang`);
+
+listaNomes = [`Enzo`, `Isabella`, `Giovanna`];
+console.log(listaNomes[0]);
+console.log(listaNomes[1]);
+console.log(listaNomes[listaNomes.length - 1]);
+
