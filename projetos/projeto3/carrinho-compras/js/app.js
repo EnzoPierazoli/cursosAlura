7026ff76fcefbc54c4a0ecf45850b9821c48dec7
Enzo Pierazoli Guerra De Moraes <enzopierazoli@gmail.com>
2025-02-12 19:26:55 -0300

    desafio3 - add carrinho-compras (apenas funções)

---
+++ projetos/projeto3/carrinho-compras/js/app.js
@@ -0,0 +1,55 @@
+let totalCarrinho = 0
+document.getElementById('lista-produtos').innerHTML = '';
+document.getElementById('valor-total').textContent = 'R$ 0';
+
+function adicionar() {
+    // recuperar valores (nome do podruto, qnt, valor)
+
+    const { nomeProduto, quantidade, valorUnitario } = recuperarDados();
+
+    //calcular o preco
+
+    calcularValorTotal(valorUnitario, quantidade);
+
+    // add no carrinho
+
+    adicionarCarrinho(nomeProduto, quantidade, valorTotal);
+
+    // atualiar o valor total
+
+    atualizarValorTotal();
+
+    // zerar contador de quantidade
+
+    document.getElementById('quantidade').value = 0
+
+}
+
+function limpar() {
+    totalCarrinho = 0
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0'; 
+}
+
+function recuperarDadosHTML() {
+    let Produto = document.getElementById('produto').value;
+    let nomeProduto = Produto.split('-')[0];
+    let valorUnitario = Produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    return { nomeProduto, quantidade, valorUnitario };
+}
+
+function calcularValorTotal(valorUnitario, quantidade) {
+    return valorTotal = parseInt(valorUnitario * quantidade);
+}
+
+function adicionarCarrinho(nomeProduto, quantidade, valorTotal) {
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto"><span class="texto-azul">${quantidade}x</span> ${nomeProduto}<span class="texto-azul"> R$${valorTotal}</></section>`;
+}
+
+function atualizarValorTotal() {
+    totalCarrinho = totalCarrinho + valorTotal
+    let valorTotalCarrinho = document.getElementById('valor-total');
+    valorTotalCarrinho.textContent = `R$ ${totalCarrinho}`;
+}
